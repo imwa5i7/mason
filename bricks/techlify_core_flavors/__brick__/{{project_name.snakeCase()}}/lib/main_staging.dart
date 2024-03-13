3f89a7ee4f6098ef31ec978400c44f92d901a986
Wasil Khan <wasil@techlify.com>
2024-03-13 22:50:22 +0500

    minnor changes in TCF brick

--- bricks/techlify_core_flavors/__brick__/{{project_name.snakeCase()}}/lib/main_staging.dart
+++ bricks/techlify_core_flavors/__brick__/{{project_name.snakeCase()}}/lib/main_staging.dart
@@ -2,5 +2,5 @@ import 'package:{{project_name.snakeCase()}}/app/app.dart';
 import 'package:{{project_name.snakeCase()}}/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  runApp(() => const App());
 }
