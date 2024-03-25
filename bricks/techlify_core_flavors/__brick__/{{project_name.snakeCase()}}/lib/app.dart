c38f4445834d270a2e88d0bb31095900a66aca17
Wasil Khan <wasil@techlify.com>
2024-03-25 23:02:10 +0500

    core updated

--- bricks/techlify_core_flavors/__brick__/{{project_name.snakeCase()}}/lib/app.dart
+++ bricks/techlify_core_flavors/__brick__/{{project_name.snakeCase()}}/lib/app.dart
@@ -8,11 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size({
-        {screen_width}
-      }, {
-        {screen_height}
-      }),
+      designSize: const Size("{{screen_width}}", "{{screen_height}}"),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
