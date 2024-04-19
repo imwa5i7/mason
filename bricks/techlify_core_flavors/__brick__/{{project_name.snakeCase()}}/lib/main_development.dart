d434784eb35d6dbbacd3683dd0429cb8283e343f
Wasil Khan <wasil@techlify.com>
2024-04-19 23:38:09 +0500

    tech_core brick updated

--- bricks/techlify_core_flavors/__brick__/{{project_name.snakeCase()}}/lib/main_development.dart
+++ bricks/techlify_core_flavors/__brick__/{{project_name.snakeCase()}}/lib/main_development.dart
@@ -3,5 +3,5 @@ import 'package:{{project_name.snakeCase()}}/bootstrap.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(() => const ProviderScope(child: App()));
 }
